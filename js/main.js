28b5deef4aa5c230891eed7a1961198eb8837151
EvgeniyLazoriv <EvgeniyLazoriv@yandex.com>
2020-11-18 15:24:59 +0200

    15:25

--- js/main.js
+++ js/main.js
@@ -16,6 +16,49 @@ $(document).ready(function(){
         }
     ]
   });
+
+  $('.gallery-slider').slick({
+    arrows: true,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: false,
+		            /* Адаптивность */
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              arrows: false
+          }    
+        },
+        {
+          breakpoint: 576,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false
+          }    
+        }
+
+    ]
+  });
+
+  new WOW().init();
   
+  /* Arctick Modal */
+
+  $('.popup__btn').click(function(e) {
+    e.preventDefault();
+    $('#popup').arcticmodal();
+});
   
 });
